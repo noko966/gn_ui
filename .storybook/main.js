b500e904509ab09df4bc787093740e76a9ad6a03
noko966 <kananovn@gmail.com>
2024-03-28 11:34:00 +0400

    storybook integrated

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+    brandImageL: "https://sport.totogaming.am/Img/partners/2/tg_logo.png",
+  },
+};
+export default config;
---
+++ .storybook/manager.js
@@ -0,0 +1,13 @@
+import { addons } from "@storybook/manager-api";
+import { create } from "@storybook/theming";
+
+addons.setConfig({
+  theme: create({
+    base: "dark",
+    brandTitle: "Digitain Component Library",
+    brandUrl: "https://www.digitain.com/",
+    brandImage:
+      "https://www.digitain.com/wp-content/themes/digitain_wp_theme/assets/img/digitain_logo.png",
+    brandTarget: "_self",
+  }),
+});
