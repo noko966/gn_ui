f2bc66e7799df100b862e7f05b8546eac48f03a6
noko966 <kananovn@gmail.com>
2024-04-19 09:35:17 +0400

    asd

--- src/lib/components/Layouts/Root/-moleculs/European/index.js
+++ src/lib/components/Layouts/Root/-moleculs/European/index.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import "./index.scss";
 
 const EuropeanView = ({ children, fullHeight, view, isRTL }) => {
+  const rootRef = useRef(null);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const updateWidth = () => {
+      if (rootRef.current) {
+        setWidth(rootRef.current.offsetWidth);
+      }
+    };
+
+    window.addEventListener("resize", updateWidth);
+    updateWidth(); // Initial call
+
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
+  const getClass = () => {
+    if (width < 600) {
+      return "m";
+    } else if (width >= 600 && width < 900) {
+      return "m t";
+    } else if (width >= 900 && width < 1200) {
+      return "m t ds";
+    } else if (width >= 1200 && width < 1600) {
+      return "m t ds dm";
+    } else {
+      return "m t ds dm dl";
+    }
+  };
+
   const ViewClassName = classNames({
     [`view_european_${"root"}`]: true,
     dg_rtl: isRTL,
     view_european_scroll_y_full_height: fullHeight,
+    [getClass()]: true,
   });
   return (
-    <main view={view} isRTL={isRTL} className={ViewClassName}>
+    <main view={view} isRTL={isRTL} className={ViewClassName} ref={rootRef}>
       {children}
     </main>
   );
